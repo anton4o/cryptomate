961084cbdbc178a3b5ed69db2579ae845262f45a
Anton Antonov <tony@antonantonov.co.uk>
2017-07-10 22:34:57 +0100

    log ccy input

--- src/main/java/util/CryptoMateUtils.java
+++ src/main/java/util/CryptoMateUtils.java
@@ -1,8 +1,15 @@
 package util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CryptoMateUtils {
 
+    private static final Logger log = LoggerFactory.getLogger(CryptoMateUtils.class);
+
     public static String getCurrencyName(String ccyInput) {
+        log.info("ccyInput: {}", ccyInput);
+
         if (ccyInput == null || ccyInput.isEmpty()) {
             return null;
         }
